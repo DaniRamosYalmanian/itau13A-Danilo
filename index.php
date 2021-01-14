9614254f7269f4276e3283451a9c656303d81bc6
DaniRamosYalmanian <danielaramosyalmanian@gmail.com>
2021-01-14 15:03:41 -0300

    link whatsapp

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <h1>"No amor e na fé encontraremos as forças necessárias para a nossa missão"</h1>
 Irmã Dulce.
+
+<a href="https://api.whatsapp.com/send?phone=551199999999&text=Entre%20em%20contato%20para%20comprar%20nossos%20servi%C3%A7os.">Entre em contato para receber maiores informações.</a>
